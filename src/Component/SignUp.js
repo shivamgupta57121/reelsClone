51d8fdcc3c23ec84bb99c9668a44597193d96cf8
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-07-11 19:42:10 +0530

    Profile Image Upload

--- src/Component/SignUp.js
+++ src/Component/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { AuthContext } from '../Context/AuthProvider';
+import { storage, database } from '../firebase'
 
 function SignUp() {
     const [email, setEmail] = useState('');
@@ -7,15 +8,70 @@ function SignUp() {
     const [name, setName] = useState('');
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
+    const [file, setFile] = useState(null);
     const { signup } = useContext(AuthContext);
     // console.log(signup);
     const handleSignup = async (e) => {
         e.preventDefault(); // event of submission - reload the page - to prevent that
-        setLoading(true);
-        let res = await signup(email,password);
-        let uid = res.user.uid;
-        console.log(uid);
-        setLoading(false);
+        try {
+            setLoading(true);
+            let res = await signup(email, password);
+            let uid = res.user.uid;
+            console.log(uid);
+            setLoading(false);
+            const uploadTaskListener = storage.ref(`/users/${uid}/profileImage`).put(file);
+            // Register three observers:
+            // 1. 'state_changed' observer, called any time the state changes
+            // 2. Error observer, called on failure
+            // 3. Completion observer, called on successful completion
+            uploadTaskListener.on("state_changed", fn1, fn2, fn3);
+            // fn 1 -> progress tracking
+            // fn2 -> error
+            // fn3 -> succes
+            function fn1(snapshot) {
+                var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                // console.log('Upload is ' + progress + '% done');
+                console.log(`Upload is ${progress}% done`);
+            }
+            function fn2(error) {
+                setError(error);
+                setTimeout(() => {
+                    setError('');
+                }, 2000);
+                setLoading(false);
+                // sign up reverse since profile image to be done self
+            }
+            async function fn3() {
+                // Handle successful uploads on complete
+                // Get the download URL
+                let downloadUrl = await uploadTaskListener.snapshot.ref.getDownloadURL();
+                console.log(downloadUrl);
+                await database.users.doc(uid).set({
+                    email: email,
+                    userId: uid,
+                    username: name,
+                    createdAt: database.getCurrentTimeStamp(),
+                    profileUrl: downloadUrl,
+                    postIds: []
+                })
+                setLoading(false);
+                console.log('User has Signed Up.')
+            }
+        }
+        catch (err) {
+            setError(err);
+            setTimeout(() => {
+                setError('');
+            }, 2000);
+            setLoading(false);
+        }
+    }
+    const handleFileSubmit = (e) => {
+        let file = e.target.files[0]; // e.target.files gives an array, file is present at 0th index 
+        console.log(file);
+        if (file != null) {
+            setFile(file);
+        }
     }
     return (
         <div>
@@ -31,6 +87,10 @@ function SignUp() {
                 <div>
                     <label htmlFor=''>Password</label>
                     <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor='profile'>Profile Image</label>
+                    <input type='file' accept='image/*' onChange={handleFileSubmit} />
                 </div>
                 <button type='submit' disabled={loading}>Submit</button>
             </form>
