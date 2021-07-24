baf127d432d0d9e0cc1d374f050772d0a2effff8
shivamgupta57121 <shivamgupta57121@gmail.com>
2021-07-24 15:35:27 +0530

    Upload Post

--- src/Component/UploadFile.js
+++ src/Component/UploadFile.js
@@ -3,18 +3,80 @@ import { makeStyles } from '@material-ui/core/styles';
 import Alert from '@material-ui/lab/Alert';
 import { Button } from '@material-ui/core';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import {v4 as uuidv4} from 'uuid'
+import { database, storage } from '../firebase';
 
 const useStyles = makeStyles((theme) => ({
 
 }));
 
-function UploadFile() {
+function UploadFile(props) {
     const classes = useStyles();
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const types = ['video/mp4', 'video/webm', 'video/ogg'];
     const onChange = (e) => {
+        const file = e?.target?.files[0];
+        console.log(file);
+        if (!file) {
+            setError('Please select a file');
+            setTimeout(() => { setError(null) }, 2000)
+            return;
+        }
 
+        if (types.indexOf(file.type) == -1) {
+            setError('Please select a video file');
+            setTimeout(() => { setError(null) }, 2000)
+            return;
+        }
+
+        if (file.size / (1024 * 1024) > 100) {
+            setError('The selected file is too big');
+            setTimeout(() => { setError(null) }, 2000)
+            return;
+        }
+
+        const id = uuidv4();
+        console.log(props.userData);
+        const uploadTask = storage.ref(`/posts/${props.userData.userId}/${file.name}`).put(file);
+        uploadTask.on('state_changed', fn1, fn2, fn3);
+        function fn1(snapshot) {
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+        }
+        function fn2(error) {
+            setError(error);
+            setTimeout(() => { setError(''); }, 2000);
+            setLoading(false);
+        }
+        function fn3() {
+            setLoading(true);
+            uploadTask.snapshot.ref.getDownloadURL().then(url => {
+                let obj = {
+                    comments: [],
+                    likes: [],
+                    pId: id,
+                    pUrl: url,
+                    uName: props.userData.username,
+                    uProfile: props.userData.profileUrl,
+                    userId: props.userData.userId,
+                    createdAt: database.getCurrentTimeStamp()
+                }
+                console.log(obj);
+                database.posts.add(obj).then(async docRef => {
+                    console.log(docRef);
+                    let res = await database.users.doc(props.userData.userId).update({
+                        postIds: [...props.userData.postIds, docRef.id]
+                    });
+                }).then(() => {
+                    setLoading(false);
+                }).catch(e => {
+                    setError(e);
+                    setTimeout(() => { setError(null) }, 2000);
+                    setLoading(false)
+                })
+            })
+        }
     }
     return (
         <>
